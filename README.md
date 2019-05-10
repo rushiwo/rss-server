dd635872c1d23687e701ced6a02ef517ea48135d
chxwang <rushiwo@126.com>
2019-05-10 13:02:58 +0800

    增加异常检测，解决线程退出问题

--- README.md
+++ README.md
@@ -5,11 +5,12 @@
 2. flask
 3. BeautifuSoup
 4. requests
-
+5. lxml
+6. pytz
 ## 使用方法1
-1. 修改`server.py`里的passkey、username、password
-2. `python server.py`
-
+1. 修改`server.py`里的passkey、username、password、url
+2. `python server.py`（直接执行，可能会异常退出）
+3. 用`supervisor`管理（推荐）
 rss地址：http://yourip:5000/rss
 
 ## 使用方法2
--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import os
+import pytz
 import requests
 from flask import Flask
 from flask import Response
@@ -11,28 +13,62 @@ from threading import Thread,Lock
 logname = ""
 password = ""
 payload = {'username':logname,'password':password}
-        # print(r.text)
 s = requests.Session()
 # start=time.time_ns()
 url='https://www.tjupt.org/torrents.php?incldead=0&spstate=2&picktype=0&inclbookmarked=0&search=&search_area=0&search_mode=0'
-all_torrents=[]
 rss_doc=''
+time.sleep(120)
 r = s.post("https://www.tjupt.org/takelogin.php",data=payload,timeout=10)
-items=[]
 lock = Lock()
 def genrss():
 
     global url, all_torrents, rss_doc, s, lock
     while True:
-        html_doc = s.get(url,timeout=10)
+        print("begin fetch html")
+        try:
+            html_doc = s.get(url,timeout=10)
+            print("status_code:"+str(html_doc.status_code))
+            if html_doc.status_code != requests.codes.ok:
+                print("requests status code not 200 OK")
+                time.sleep(60)
+                continue
+        except requests.exceptions.HTTPError as errh:
+            print ("Http Error:",errh)
+            time.sleep(60)
+            continue
+        except requests.exceptions.ConnectionError as errc:
+            print ("Error Connecting:",errc)
+            time.sleep(60)
+            continue
+        except requests.exceptions.Timeout as errt:
+            print ("Timeout Error:",errt)
+            time.sleep(60)
+            continue
+        except requests.exceptions.RequestException as err:
+            print ("OOps: Something Else",err)
+            os._exit(0)
         soup = BeautifulSoup(html_doc.text,'lxml')
-        torrents = soup.find_all("table","torrents")[0]
+#        print(html_doc.text)
+        try:
 
+            table_t = soup.find_all("table","torrents")
+            if len(table_t)==0:
+                 print("table of torrents not found ")
+                 time.sleep(60)
+                 continue
+            #print(len(table_t))
 
+            torrents = table_t[0]
+        except IndexError as err:
+            print("IndexError:",err)
+            os._exit(0)
+        local_torrents=[]
+        items=[]
+        print("soup finding")
         for item in torrents.tr.find_next_siblings('tr'):
             all_links = item.table.tr.find_all("a")
             title = all_links[0]['title']
-            enclosure = 'https://www.tjupt.org/'+all_links[1]['href']+'&passkey='
+            enclosure = 'https://www.tjupt.org/'+all_links[1]['href']+'&passkey='+''
             link = 'https://www.tjupt.org/'+all_links[0]['href']
             size=0
             seeders=0
@@ -65,7 +101,7 @@ def genrss():
                 total_downloader = 0
             else:
                 total_downloader = int(td.a.b.string.replace(',',''))
-            all_torrents.append({
+            local_torrents.append({
                 'title':title,
                 'enclosure':enclosure,
                 'link':link,
@@ -74,54 +110,35 @@ def genrss():
                 'total_downloader':total_downloader,
                 'size':size
             })
-            k=1
-        for t in all_torrents:
-            des=s.get(t['link'])
-            print(k)
-            k=k+1
+            #k=1
+        for t in local_torrents:
+            #des=s.get(t['link'])
+            #print(k)
+            #k=k+1
             #soup = BeautifulSoup(des.text,'lxml')
-            has = re.search(r"◎简(.|\n)*<font",des.text,re.M)
+            #has = re.search(r"◎简(.|\n)*<font",des.text,re.M)
             #print(has)
-            if has:
-                description = has.group()[0:-5]
-            else:
-                description = '无简介'
+            #if has:
+            #    description = has.group()[0:-5]
+            #else:
+            description = '无简介'
             items.append(Item(title = t['title'],
-                              link = t['link'],
-                              description = description.replace("<br />","\n").replace("&nbsp;","").strip(),
-                              enclosure = Enclosure(url =t['enclosure'] , length = t['size'], type = 'application/x-bittorrent')
-                              ))
-
+                             link = t['link'],
+                             description = description.replace("<br />","\n").replace("&nbsp;","").strip(),
+                             enclosure = Enclosure(url =t['enclosure'] , length = t['size'], type = 'application/x-bittorrent')
+                             ))
+        tz_sh = pytz.timezone('Asia/Shanghai')
         feed = Feed(title = "北洋园PT Free Torrents",
                      link = "https://www.tjupt.org",
                      description = "Latest free torrents from 北洋园PT",
                      language = "zh-cn",
-                     lastBuildDate = datetime.datetime.now(),
+                     lastBuildDate = datetime.datetime.now(tz_sh),
                      items = items)
         lock.acquire()
         rss_doc = feed.rss()
         lock.release()
-        time.sleep(120)
+        time.sleep(60)
 
-        # print(html_doc.text)
-        # stop = time.time_ns()
-        # print("time:"+str((stop-start)/(10**9)))
-
-        # lock = Lock()
-
-        # def hello():
-
-        #     while True:
-
-
-        #         lock.acquire()
-
-        #         lock.release()
-        #         time.sleep(1)
-
-        # t1 = Thread(target=hello,daemon=True)
-        # t1.start()
-        # print(r.text)
 
 t1 = Thread(target=genrss,daemon=True)
 t1.start()
@@ -134,4 +151,5 @@ def rss():
     return resp
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
 
